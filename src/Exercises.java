feda7fb1f1f44c71a24927367f53ce7bf649183f
Alexander Vasquez <Acexander0@gmail.com>
2020-03-22 15:38:33 -0400

    Grader fixed and Exercise 3 complete

--- src/Exercises.java
+++ src/Exercises.java
@@ -1,5 +1,4 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Exercises {
 	 
@@ -31,36 +30,29 @@ public class Exercises {
 	  return -1;  
   }
 
-  public int findMeFaster(ArrayList<Integer> list, int target) {
-	if (list == null || list.size() == 0) {
-		return -1; 
-	}
-	
-	int lower = 0;
-	int upper = list.size() - 1;
-	int mid = list.size() / 2; 
-	
-	while  (lower <= upper) {
-		if (target == list.get(lower)) {
-			return lower;
-		} 
-		
-		if	(target == list.get(upper)) {
-			return upper;
+	public int findMeFaster(ArrayList<Integer> list, int target) {
+		if (list == null) {
+			return -1; 
 		}
 		
-		if (target > list.get(mid)) {
-			lower = mid;
-			mid = (lower + upper) / 2;
-		} else if (target < list.get(mid)) {
-			upper = mid;
-			mid = (lower + upper) / 2;
-		} else {
-			return mid;
+		int lower = 0;
+		int mid = 0; 
+		int upper = list.size();
+		
+		
+		while  (lower <= upper) {
+			mid = (upper + lower) / 2; 
+
+			if (target > list.get(mid)) {
+				lower = mid + 1;
+			} else if (target < list.get(mid)) {
+				upper = mid - 1;
+			} else {
+				return mid;
+			}
 		}
+	    return -1;
 	}
-    return -1;
-  }
 
   public int findMeFaster(String[] list, String target) {
 	  if (list == null || list.length == 0) {
@@ -69,24 +61,17 @@ public class Exercises {
 		
 		int lower = 0;
 		int upper = list.length - 1;
-		int mid = list.length / 2; 
+		int mid = list.length / 2;
 		
-		while  (lower <= upper) {
-			if (target.equals(list[lower])) {
-				return lower;
-			} 
+		while  (lower < mid) {
 			
-			if	(target == list[upper]) {
-				return upper;
-			}
-			
-			if (target > list[mid]) {
+			if (0 > list[mid].compareTo(target)) {
 				lower = mid;
 				mid = (lower + upper) / 2;
-			} else if (target < list.get(mid)) {
+			} else if (0 < list[mid].compareTo(target)) {
 				upper = mid;
 				mid = (lower + upper) / 2;
-			} else {
+			} else if (0 == list[mid].compareTo(target)) {
 				return mid;
 			}
 		}
